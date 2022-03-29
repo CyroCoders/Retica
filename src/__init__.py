ccb8648ed4e6cde3a3a687895d908e5170c29d1d
Sushant Shah <blckblze@gmail.com>
2022-03-29 17:15:07 +0530

    Made Server Windows-Safe

--- src/__init__.py
+++ src/__init__.py
@@ -1,28 +1,11 @@
-from . import Worker
-import os, sys
+from . import Worker, Request, Response
+import os, sys, select
+import threading
+import logging
 
 class Server:
-    def __init__(self, context):
-        if os.name == 'nt':
-            print(os.path.abspath(__file__))
-            wsl = True if input("Windows still doesn't support fork(), Y/N to use with Windows Subsystem for Linux(WSL)").lower() == "y" else False
-            if wsl:
-                import __main__
-                import subprocess
-                if subprocess.run(["wsl", "test", "-f", "/etc/os-release"]).returncode == 0:
-                    subprocess.run(["wsl", "python3", os.path.abspath(__file__), "--wsl"])
-                else:
-                    install = input("WSL not found. Install WSL? Y/N")
-                    if install.lower() == "y":
-                        # if subprocess.run(["DISM", "/Online", "/Enable-Feature", "/All", "/FeatureName:HypervisorPlatform"]).returncode == 0:
-                        subprocess.run(["wsl", "--install", "-d", "Ubuntu"])
-                        if subprocess.run(["wsl", "test", "-f", "/etc/os-release"]).returncode == 0:
-                            subprocess.run(["wsl", "python3", os.path.abspath(__file__), "--wsl"])
-                        else:
-                            print("Please Turn On Virtualization In BIOS And Try Again. If The Problem Persists, Please Install WSL Manually.")
-                        # else:
-                        #     print("Please Run Script As Administrator. Or Run 'DISM /Online /Enable-Feature /All /FeatureName:HypervisorPlatform' In Command Prompt With Administrator Privileges.")
-
+    def __init__(self, context, config=None):
+        self.config = config
         self.context = context
         self.endpoints = {}
         self.workers = []
@@ -37,19 +20,25 @@ class Server:
         return wrapper
 
     def run(self, sockets):
-        for socket_index in range(len(sockets)):
-            worker = Worker.Worker(self)
-            pid = os.fork()
-            if pid == 0:
-                while True:
-                    request = sockets[socket_index].recv(1024)
-                    if not request:
-                        break
-                    worker.handle(sockets[socket_index], request)
-                    sockets[socket_index].close()
-            else:
-                self.workers.append(pid)
-        
+        for socket in sockets:
+            socket.bind()
+        while True:
+            read_socks,_,_ = select.select(sockets, [], [])
+            for sock in read_socks:
+                self.workers.append(threading.Thread(target=self.handle_request, args=(*sock.sock.accept(),)))
+                self.workers[-1].start()
+            read_socks = []
+
+    def handle_request(self, socket, address):
+        logging.info(f"New Connection From {address}")
+        request = Request.request()
+        request.parse(socket.recv(1024))
+        worker = Worker.Worker(self)
+        if not request:
+            return
+        worker.handle(socket, request)
+        socket.close()
+
     def kill(self):
         for worker in self.workers:
             os.kill(worker, 9)
