ddd290a35a9576c5e43a9f9ece1aa82c05c06071
Sushant Shah <blckblze@gmail.com>
2022-03-30 14:30:19 +0530

    Added Basic Plugin Class

---
+++ src/CyroWebB/Plugin.py
@@ -0,0 +1,13 @@
+class Plugin(object):
+    def __init__(self, server, config={}):
+        self.server = server
+        self.config = config
+
+    def intercept_request(self, request):
+        pass
+
+    def intercept_response(self, response):
+        pass
+
+    def run(self):
+        pass
