e3d33028f5e0ed28ec5ca98f20ded4b6fbd16d65
Sushant Shah <blckblze@gmail.com>
2022-04-02 13:29:46 +0530

    Exceptions(Except KeyboardInterrupt) Are Now Logged As Exceptions. KeyboardExceptions Are Ignored.

--- src/Retica/Worker.py
+++ src/Retica/Worker.py
@@ -31,60 +31,65 @@ class Worker:
 
         :rtype: None
         """
-        response = Response.response()
-        path = request.path
-        path = path.decode()
-        for plugin in self.server.plugins:
-            plugin.intercept_request(request)
-        for endpoint, (handler,condition) in self.server.endpoints.items():
-            proceed = True
-            if condition is not None:
-                proceed = condition(request)
-            if proceed==True:
-                endpoint = "." + endpoint + "."
-                path = "." + path + "."
-                if endpoint[-1] == "/":
-                    endpoint = endpoint[:-1]
-                if endpoint[1] == "/":
-                    endpoint = endpoint[1:]
-                if path[-1] == "/":
-                    path = path[:-1]
-                if path[1] == "/":
-                    path = path[1:]
-                endpoint = endpoint[1:-1]
-                path = path[1:-1]
-                if parse.parse(endpoint, path):
-                    arguments = parse.parse(endpoint, path)
-                    arguments = dict(arguments)
-                    handler(request, response, **arguments)
-                    for plugin in self.server.plugins:
-                        plugin.intercept_response(response)
+        try:
+            response = Response.response()
+            path = request.path
+            path = path.decode()
+            for plugin in self.server.plugins:
+                plugin.intercept_request(request)
+            for endpoint, (handler,condition) in self.server.endpoints.items():
+                proceed = True
+                if condition is not None:
+                    proceed = condition(request)
+                if proceed==True:
+                    endpoint = "." + endpoint + "."
+                    path = "." + path + "."
+                    if endpoint[-1] == "/":
+                        endpoint = endpoint[:-1]
+                    if endpoint[1] == "/":
+                        endpoint = endpoint[1:]
+                    if path[-1] == "/":
+                        path = path[:-1]
+                    if path[1] == "/":
+                        path = path[1:]
+                    endpoint = endpoint[1:-1]
+                    path = path[1:-1]
+                    if parse.parse(endpoint, path):
+                        arguments = parse.parse(endpoint, path)
+                        arguments = dict(arguments)
+                        handler(request, response, **arguments)
+                        for plugin in self.server.plugins:
+                            plugin.intercept_response(response)
+                        socket.sendall(response.compile())
+                        return
+                elif isinstance(proceed, Response.response):
+                    response.body = proceed
+                    socket.sendall(proceed.compile())
+                    return
+                elif isinstance(proceed, str):
+                    response.body = proceed
                     socket.sendall(response.compile())
                     return
-            elif isinstance(proceed, Response.response):
-                response.body = proceed
-                socket.sendall(proceed.compile())
-                return
-            elif isinstance(proceed, str):
-                response.body = proceed
+                elif isinstance(proceed, bytes):
+                    response.body = proceed
+                    socket.sendall(proceed.compile())
+                    return
+
+            static_path = os.path.join(os.path.dirname(os.path.abspath(self.server.context)), "static",*path.split("/"))
+            if os.path.isfile(static_path):
+                mimetype = mimetypes.guess_type(static_path)
+                response.body = open(static_path, "rb").read()
+                response.headers["Content-Type"] = mimetype[0] or "application/octet-stream"
+                if mimetype[1]:
+                    response.headers["Content-Encoding"] = mimetype[1]
                 socket.sendall(response.compile())
                 return
-            elif isinstance(proceed, bytes):
-                response.body = proceed
-                socket.sendall(proceed.compile())
+            else:
+                response.status = "404"
+                response.body = "404 Not Found"
+                socket.sendall(response.compile())
                 return
-
-        static_path = os.path.join(os.path.dirname(os.path.abspath(self.server.context)), "static",*path.split("/"))
-        if os.path.isfile(static_path):
-            mimetype = mimetypes.guess_type(static_path)
-            response.body = open(static_path, "rb").read()
-            response.headers["Content-Type"] = mimetype[0] or "application/octet-stream"
-            if mimetype[1]:
-                response.headers["Content-Encoding"] = mimetype[1]
-            socket.sendall(response.compile())
-            return
-        else:
-            response.status = "404"
-            response.body = "404 Not Found"
-            socket.sendall(response.compile())
-            return
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            self.server.logger.exception(e)
