47f34fd1907398bb5282c7f5fadf2deb5747e739
Sushant Shah <sushantshah@cyrocoders.dev>
2022-06-09 05:03:24 +0530

    server_object & host Arguments Now Parsed Only In Case Of run Command

--- src/Retica/cli/__init__.py
+++ src/Retica/cli/__init__.py
@@ -6,9 +6,11 @@ from .run import run
 def main():
     parser = argparse.ArgumentParser(description="The Retica CLI. Retica Is A Performant, Easy To Use Web Server Based On Python With HTTPS & Plugins Support.")
     subparsers = parser.add_subparsers(dest='command')
-    subparsers.add_parser('run', help="Run the current project.")
-    parser.add_argument("server_object", type=str, const="app:retica", default="app:retica", nargs='?', help="Server object (app:retica)")
-    parser.add_argument("host", type=str, const="localhost:80", default="localhost:80", nargs='?', help="Hostname and port (localhost:80)")
+    
+    run_parser = subparsers.add_parser('run', help="Run the current project.")
+    run_parser.add_argument("server_object", type=str, const="app:retica", default="app:retica", nargs='?', help="Server object (app:retica)")
+    run_parser.add_argument("host", type=str, const="localhost:80", default="localhost:80", nargs='?', help="Hostname and port (localhost:80)")
+
     subparsers.add_parser('create', help="Create a new project.")
 
     args = parser.parse_args()
@@ -19,6 +21,8 @@ def main():
         case 'run':
             run(args)
             return
+        case None:
+            parser.print_help()
 
 if __name__ == "__main__":
     main()
