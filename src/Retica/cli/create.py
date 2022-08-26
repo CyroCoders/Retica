96aa51f2858e2055199c9bd5ec7b46820e4b2771
Sushant Shah <sushantshah@cyrocoders.dev>
2022-08-26 14:12:31 +0530

    app.py Now Modified With Frontend Libraries And Metadata After Tarfile Extraction

--- src/Retica/cli/create.py
+++ src/Retica/cli/create.py
@@ -27,18 +27,30 @@ def create(args):
     file = tarfile.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "resources/default-homepage.tar.gz"))
     file.extractall(os.path.join(os.getcwd(), project_name))
 
-    with open(os.path.join(project_name, "app.py"), "w") as f:
-        f.write('import Retica\n')
-        f.write('import Retica.Render\n')
-        f.write('retica = Retica.Server(__name__)\n\n')
-        f.write(f'templator = Retica.Render.TemplateRender(retica,template_dir="{templates_folder}")\n\n')
-        f.write('@retica.create_endpoint("/hello/{name}")\n')
-        f.write('def index(request: Retica.Request.request, response: Retica.Response.response, **data):\n')
-        f.write('    response.body = f"Hello {data[\'name\']}"\n\n')
 
-    with open(os.path.join(project_name, "run.py"), "w") as f:
-        f.write('from app import retica\n')
-        f.write('from Retica import Sockets\n\n')
-        f.write('http_socket = Sockets.HTTP_Socket(Sockets.gethostname(), 80)\n\n')
-        f.write('if __name__ == "__main__":\n')
-        f.write('    retica.run([http_socket])\n')
+    with open(os.path.join(os.getcwd(), project_name, "app.py"), "r") as in_data:
+        in_data = in_data.readlines()
+        
+    with open(os.path.join(os.getcwd(), project_name, "app.py"), "w") as out_data:
+        for line in in_data:
+            if line.startswith("libraries = "):
+                if frontend_libraries_selection:
+                    out_data.write("libraries = {\n")
+                    for lib in frontend_libraries_selection.split(","):
+                        print(list(frontend_libraries.keys())[0])
+                        out_data.write(f"   '{list(frontend_libraries.keys())[int(lib)]}': [\n")
+                        for script in list(frontend_libraries.values())[int(lib)]:
+                            out_data.write(f"       '{script}',\n")
+                        out_data.write("   ],\n")
+                    out_data.write("}\n")
+                else:
+                    out_data.write("libraries = {}\n")
+            elif line.startswith("metadata = "):
+                out_data.write("metadata = {\n")
+                out_data.write(f"   'name': '{project_name}',\n")
+                out_data.write(f"   'description': '{project_description}',\n")
+                out_data.write(f"   'author': '{project_author}',\n")
+                out_data.write(f"   'keywords': '{project_keywords}',\n")
+                out_data.write("}\n")
+            else:
+                out_data.write(line)
